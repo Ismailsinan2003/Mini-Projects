92665be7c22cf4ad2a51aace134727864fa850c5
Ismail Sinan <ismailsinan2003@gmail.com>
2025-08-20 17:35:32 +0400

    Ripple effect

---
+++ RippleEffect/script.js
@@ -0,0 +1,14 @@
+const button=document.querySelector(".btn")
+
+button.addEventListener('mouseover', (event)=>{
+    const xpos=event.offsetX
+    const ypos=event.offsetY
+    
+    
+    button.style.setProperty("--xpos",`${xpos}px`)
+    button.style.setProperty("--ypos", `${ypos}px`)
+
+    
+
+
+})
