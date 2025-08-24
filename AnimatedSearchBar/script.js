87f1f6a720ecc758143f027dca358de86d85fc41
Ismail Sinan <ismailsinan2003@gmail.com>
2025-08-24 17:47:47 +0400

    Animated Search Bar

---
+++ AnimatedSearchBar/script.js
@@ -0,0 +1,12 @@
+const SearchBar= document.querySelector(".search-bar-container")
+const Magnifier = document.querySelector(".magnifier")
+
+
+Magnifier.addEventListener('click', ()=>{
+    SearchBar.classList.remove("active")
+
+})
+
+
+
+
